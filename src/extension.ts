015939b4964cfb0c38b974a569f07e4c132a8496
Mohammad Hraiba <hraibam@gmail.com>
2025-07-05 13:23:35 +0200

    feat: improve status bar item behavior by removing default text and ensuring proper visibility

--- src/extension.ts
+++ src/extension.ts
@@ -2,33 +2,31 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 		const statusBarItem =	vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
-		const fileName = vscode.window.activeTextEditor?.document.fileName.split('/').pop() ?? 'No active file';
+		const fileName = vscode.window.activeTextEditor?.document.fileName.split('/').pop() ?? '';
 		statusBarItem.text = fileName;
 		statusBarItem.tooltip = 'Active File Name';
-		statusBarItem.command = 'activefilename.helloWorld';
-		statusBarItem.show();
+		fileName === '' ? statusBarItem.hide() : statusBarItem.show();
 
 		vscode.window.onDidChangeActiveTextEditor(editor => {
 			if (editor) {
-				const newFileName = editor.document.fileName.split('/').pop() ?? 'No active file';
+				const newFileName = editor.document.fileName.split('/').pop() ?? '';
 				statusBarItem.text = newFileName;
+				statusBarItem.show();
 			}else {
-				statusBarItem.text = 'No active file';
+				statusBarItem.hide();
 			}
 		}, null, context.subscriptions);
 		vscode.workspace.onDidChangeTextDocument(event => {
 			if (vscode.window.activeTextEditor && event.document === vscode.window.activeTextEditor.document) {
-				const updatedFileName = event.document.fileName.split('/').pop() ?? 'No active file';
+				const updatedFileName = event.document.fileName.split('/').pop() ?? '';
 				statusBarItem.text = updatedFileName;
+				statusBarItem.show();
 			}else {
-				statusBarItem.text = 'No active file';
+				statusBarItem.hide();
 			}
 		}, null, context.subscriptions);
-	const disposable = vscode.commands.registerCommand('activefilename.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from ActiveFileName!');
-	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push();
 }
 
 // This method is called when your extension is deactivated
